20e87ec58aacac35600960f31fa6c466fbdda12b
Andrew Yalung <yalung@usc.edu>
2019-02-27 10:23:46 -0800

    added a restaurant class

--- src/main/java/YelpApi.java
+++ src/main/java/YelpApi.java
@@ -30,22 +30,29 @@ public class YelpApi extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String url = "https://api.yelp.com/v3/businesses/search?location=NYC&term=food";
+		String query = request.getParameter("query");
+		String size = request.getParameter("size");
+		String latitude = "34.0206";
+		String longitude = "118.2854";
+		System.out.println(query + " " + size);
+		String url = "https://api.yelp.com/v3/businesses/search?latitude="+latitude+"&longitude="+longitude+"&term="+ query + "&limit=" + size + "&sort_by=distance";
+		System.out.println(url);
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("GET");
 		con.setRequestProperty("Authorization", "Bearer vA1w5vh_CQ8cDc_ro1pu9E_7fHDhQqeUUfn5f7sNKEE9ib3akOutyXVU4o9SpMGPk11y_B7hv9zTAiytKRknZpP6g9s1Klnc7TnpQqlnDIPs1qx3pPy-qRpKfNp1XHYx");
 		int responseCode = con.getResponseCode();
-		response.getWriter().println(responseCode);
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer resp = new StringBuffer();
-		while((inputLine = in.readLine()) != null) {
-			resp.append(inputLine);
+		if(responseCode == 200) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer resp = new StringBuffer();
+			while((inputLine = in.readLine()) != null) {
+				resp.append(inputLine);
+			}
+			in.close();
+			System.out.println(resp.toString());
+			response.getWriter().println(resp.toString());
 		}
-		in.close();
-		
-		response.getWriter().println(resp.toString());
 	}
 
 	/**
